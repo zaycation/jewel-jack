1cac0aa15546688c65bac28a187a5d4c2acd22a8
Ryno <rynosapien@hotmail.co.uk>
2021-05-04 21:31:14 +0100

    refactor: rebuild contributors page for code simplicity and style update

--- src/components/Contributors.js
+++ src/components/Contributors.js
@@ -1,59 +1,43 @@
 //import { Link } from "react-router-dom";
 import Axios from "axios";
-import React, { useState, useEffect, usePrevious} from "react";
+import React, {useEffect, usePrevious, useState} from "react";
 import ContributorsList from "./ContributorsList";
 
 
-
 export default function Contributors(props) {
-
     const [contributorsData, setContributorsData] = useState();
 
-
-
     const getContributorsData = (e) => {
-       Axios.get(`http://localhost:5000/contributors/`)
-            .then((response) => {
+        Axios.get(`/api/contributors/`)
+            .then(response => {
                 setContributorsData(response.data)
-               console.log('GETresponse',response)
-
             })
-
-            .catch((error) => console.log(error))
+            .catch(console.error)
     }
-    useEffect(()=>{
+
+    useEffect(() => {
         getContributorsData();
+    }, [])
 
-    },[])
-
-    
     return (
-    
-            <section className="credits">
-                <h2 className="credits__heading">Contributors</h2>
-                <div className="credits__contributors-wrapper">
-                    {contributorsData && contributorsData.map((contributorsData) => {
-                        return (
-                            <ContributorsList
-                                key={contributorsData.id}
-                                id={contributorsData.id}
-                                name={contributorsData.name}
-                                image={contributorsData.image}
-                                linkedIn={contributorsData.linkedIn}
-                                gitHub={contributorsData.gitHub}
-                                twitter={contributorsData.twitter}
-                            />
-                            
-                        )
-                    })}
-  
-                
-                </div>
-              {usePrevious}
-            </section>
+        <section className="credits">
+            <h2>Contributors</h2>
+            <div className="d-flex flex-wrap">
+                {contributorsData && contributorsData.map(contributorsData => {
+                    return (
+                        <ContributorsList
+                            key={contributorsData.name}
+                            name={contributorsData.name}
+                            image={contributorsData.image}
+                            linkedIn={contributorsData.linkedIn}
+                            gitHub={contributorsData.gitHub}
+                            twitter={contributorsData.twitter}
+                        />
 
-
-       
+                    )
+                })}
+            </div>
+            {usePrevious}
+        </section>
     )
-
 }
